c40355e891a2c06c4a4221e95036d14971c400ed
PraDeepKuMaR <97429335+pradeepkumar24rk@users.noreply.github.com>
2023-04-02 23:02:52 +0530

    router part is added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,26 +1,26 @@
-// import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-
-// import SuccessPayment from "./pages/SuccessPayment";
-
-import Payment from "./pages/Payment";
-
-// import Product from "./pages/Product";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import Cart from "./pages/Cart";
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Home from './pages/Home'
+import ProductList from './pages/ProductList'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
-  return <div>
-    {/* <Home/> */}
-    {/* <ProductList/> */}
-    {/* <Product/> */}
-    {/* <Login/> */}
-    {/* <Register/> */}
-    {/* <Cart/> */}
-    <Payment/>
-    {/* <SuccessPayment/> */}
-  </div>;
+  const user=true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={user ? <Navigate to='/'/> :<Login/>}/>
+        <Route path='/products/:category' element={<ProductList/>}/>
+        <Route path='/product/:id' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
